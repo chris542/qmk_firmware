bf00d0c5f4d45b5d8a090d9f9ae65726b15f8e98
Chris Kang(Xero) <chris.kang@xero.com>
2023-05-02 13:46:02 +1200

    Remove desktop bin if exists

--- cmp.sh
+++ cmp.sh
@@ -2,6 +2,9 @@ echo "CK MOONLANDER COMPILING............"
 make clean && make moonlander:ckMoonlander
 
 if [ -e ./.build/moonlander_ckMoonlander.bin ]; then
+    if [ -e ~/Desktop/moonlander_ckMoonlander.bin ]; then
+        rm ~/Desktop/moonlander_ckMoonlander.bin
+    fi
     cp ./.build/moonlander_ckMoonlander.bin ~/Desktop/
     open ~/Desktop
     echo "DONE! :)"
